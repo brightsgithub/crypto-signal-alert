c52d1b245c2b5ba0c949dc147ee94de1ce86672b
brightsgithub <trainstationalert@gmail.com>
2022-07-16 07:43:52 +0100

    mocktests and real server tests working

--- data/src/androidTest/java/com/owusu/cryptosignalalert/data/datasource/coingecko/CoinGeckoDataSourceImplTest.kt
+++ data/src/androidTest/java/com/owusu/cryptosignalalert/data/datasource/coingecko/CoinGeckoDataSourceImplTest.kt
@@ -1,6 +1,5 @@
 package com.owusu.cryptosignalalert.data.datasource.coingecko
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.owusu.cryptosignalalert.data.BaseCoreTest
 import com.owusu.cryptosignalalert.data.test.R
 import com.owusu.cryptosignalalert.data.datasource.CoinsDataSource
@@ -10,28 +9,42 @@ import okhttp3.mockwebserver.MockResponse
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import java.lang.StringBuilder
 
 class CoinGeckoDataSourceImplTest : BaseCoreTest(), KoinComponent {
 
-    //lateinit var coinsDataSource: CoinsDataSource
     val coinsDataSource : CoinsDataSource by inject()
     val endPoints : EndPoints by inject()
 
     @Before
     override fun setUp() {
         super.setUp()
-
-        //loadKoinModules(overrideModule)
-
     }
 
     @After
     override fun tearDown() {
         super.tearDown()
+    }
+
+    @Test
+    fun getSingleItemInListWithMarketDataTest() = runBlocking {
+
+        val page = 1
+        val recordsPerPage = 1
+        val currencies = "usd"
+
+        // http://localhost:8080/api/v3/coins/marketsvs_currencyusdordermarket_cap_descper_page1page1sparklinefalse
+        // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1&sparkline=false
+        initDispatcher(getResponseMapForCoinsListWithMarketData(page, recordsPerPage, currencies, R.raw.get_single_coin))
+        val coinsAPIList = coinsDataSource.getCoinsList(page, recordsPerPage, currencies)
+
+        assert(coinsAPIList.isNotEmpty())
+        assert(coinsAPIList.size == recordsPerPage)
+        assert(coinsAPIList[0].id != null)
+        assert(coinsAPIList[0].name != null)
+        assert(coinsAPIList[0].currentPrice != null)
     }
 
     @Test
@@ -43,15 +56,20 @@ class CoinGeckoDataSourceImplTest : BaseCoreTest(), KoinComponent {
 
         // http://localhost:8080/api/v3/coins/marketsvs_currencyusdordermarket_cap_descper_page100page1sparklinefalse
         // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
-        initDispatcher(getResponseMapForCoinsListWithMarketData(page, recordsPerPage, currencies))
+        initDispatcher(getResponseMapForCoinsListWithMarketData(page, recordsPerPage, currencies, R.raw.get_coins_list_with_market_data))
         val coinsAPIList = coinsDataSource.getCoinsList(page, recordsPerPage, currencies)
 
         assert(coinsAPIList.isNotEmpty())
+        assert(coinsAPIList.size == recordsPerPage)
+        assert(coinsAPIList[0].id != null)
+        assert(coinsAPIList[0].name != null)
+        assert(coinsAPIList[0].currentPrice != null)
     }
 
     private fun getResponseMapForCoinsListWithMarketData(page: Int,
                                                          recordsPerPage: Int,
-                                                         currencies: String): Map<String, MockResponse> {
+                                                         currencies: String,
+                                                         mockResIdFile: Int): Map<String, MockResponse> {
 
         val hostName = endPoints.getHostName()
 
@@ -65,7 +83,10 @@ class CoinGeckoDataSourceImplTest : BaseCoreTest(), KoinComponent {
         request.append("&", "page", "=", page.toString())
         request.append("&", "sparkline", "=", false.toString())
 
-        val response = getMockedResponse(OK, R.raw.get_coins_list_with_market_data)
+        val response = getMockedResponse(OK, mockResIdFile)
+        // also mock critical RESPONSE headers!
+        response.addHeader("content-type", "application/json; charset=utf-8") // critical!
+        response.addHeader("cache-control", "public,max-age=300") // optional
         return mapOf(request.toString() to response)
     }
 }
--- data/src/main/java/com/owusu/cryptosignalalert/data/datasource/coingecko/CoinGeckoDataSourceImpl.kt
+++ data/src/main/java/com/owusu/cryptosignalalert/data/datasource/coingecko/CoinGeckoDataSourceImpl.kt
@@ -1,15 +1,12 @@
 package com.owusu.cryptosignalalert.data.datasource.coingecko
 
-import android.util.Log
 import com.owusu.cryptosignalalert.data.datasource.CoinsDataSource
 import com.owusu.cryptosignalalert.data.endpoints.EndPoints
 import com.owusu.cryptosignalalert.data.models.CoinAPI
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.decodeFromString
+import io.ktor.http.*
 
 class CoinGeckoDataSourceImpl(
     private val httpClient: HttpClient,
@@ -22,26 +19,9 @@ class CoinGeckoDataSourceImpl(
         currencies: String
     ) : List<CoinAPI> {
 
-        // vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
-
-
-//        val coinApiListStr1: String = httpClient.get(endPoints.getCoinsListWithMarketDataPath()) {
-//            // accept(ContentType.Application.Json)
-//            // contentType(ContentType.Application.Json)
-//            url {
-//                parameters.append("vs_currency", currencies)
-//                parameters.append("order", "market_cap_desc")
-//                parameters.append("per_page", recordsPerPage.toString())
-//                parameters.append("page", page.toString())
-//                parameters.append("sparkline", false.toString())
-//            }
-//
-//        }.bodyAsText()
-//
-//        Log.v("CoinGeckoDataSourceImpl", "coinApiListStr1 = "+ coinApiListStr1)
         val coinApiListStr: List<CoinAPI> = httpClient.get(endPoints.getCoinsListWithMarketDataPath()) {
-            // accept(ContentType.Application.Json)
-            // contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
             url {
                 parameters.append("vs_currency", currencies)
                 parameters.append("order", "market_cap_desc")
@@ -51,25 +31,6 @@ class CoinGeckoDataSourceImpl(
             }
 
         }.body()
-
-        //val coinApiList = Json{ ignoreUnknownKeys = true }.decodeFromString<List<CoinAPI>>(coinApiListStr)
         return coinApiListStr
-
-
-//        val coinApiListStr: String = httpClient.get(endPoints.getCoinsListWithMarketDataPath()) {
-//            // accept(ContentType.Application.Json)
-//            // contentType(ContentType.Application.Json)
-//            url {
-//                parameters.append("vs_currency", currencies)
-//                parameters.append("order", "market_cap_desc")
-//                parameters.append("per_page", recordsPerPage.toString())
-//                parameters.append("page", page.toString())
-//                parameters.append("sparkline", false.toString())
-//            }
-//
-//        }.body()
-//
-//        val coinApiList = Json{ ignoreUnknownKeys = true }.decodeFromString<List<CoinAPI>>(coinApiListStr)
-//        return coinApiList
     }
 }

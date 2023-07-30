933278fb74863e99c1265c7102f97b904e4d173f
BrightPersonal <brightpersonal@Brights-Mac-mini.local>
2023-07-30 21:00:31 +0100

    siren setting added

--- presentation/src/main/java/com/owusu/cryptosignalalert/viewmodels/SharedViewModel.kt
+++ presentation/src/main/java/com/owusu/cryptosignalalert/viewmodels/SharedViewModel.kt
@@ -124,7 +124,7 @@ class SharedViewModel(
 
     private fun attemptToShowInterstitialAd() {
         Log.d(TAG, "attemptToShowInterstitialAd called")
-        callFunctionWithProbability(probability = 0.08) {
+        callFunctionWithProbability(probability = 0.1) {
             Log.d(TAG, "shouldShowInterstitialAd = true")
             _sharedViewState.value = _sharedViewState.value.copy(
                 shouldShowInterstitialAd = true
